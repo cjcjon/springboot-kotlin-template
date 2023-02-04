6973d7c297d08a82a91dff75ebecd88110799bd8
cjcjon <cjcjon30@gmail.com>
2023-02-04 14:25:44 +0900

    initial commit

---
+++ src/main/kotlin/microservices/book/socialmultiplication/SocialMultiplicationApplication.kt
@@ -0,0 +1,11 @@
+package microservices.book.socialmultiplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMultiplicationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialMultiplicationApplication>(*args)
+}
