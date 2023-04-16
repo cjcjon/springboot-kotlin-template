cjcjon/issue-manage

build: 스프링 기본 설정

settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "social-multiplication"
+rootProject.name = "issue-manage"

src/test/kotlin/com/spring/SpringApplicationTests.kt
@@ -1,0 +1,12 @@
+package com.spring
+
+import com.spring.test.SpringIntegrationBDDTest
+import io.kotest.matchers.shouldBe
+
+class SpringApplicationTests : SpringIntegrationBDDTest({
+  feature("스프링을 실행한다") {
+    scenario("스프링의 context가 정상적으로 불러와진다") {
+      true shouldBe true
+    }
+  }
+})

cjcjon/springboot-kotlin-template

settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "social-multiplication"
+rootProject.name = "spring-project"

src/test/kotlin/com/spring/SpringApplicationTests.kt
@@ -1,0 +1,12 @@
+package com.spring
+
+import com.spring.test.SpringIntegrationBDDSpec
+import io.kotest.matchers.shouldBe
+
+class SpringApplicationTests : SpringIntegrationBDDSpec({
+  feature("스프링을 실행한다") {
+    scenario("스프링의 context가 정상적으로 불러와진다") {
+      true shouldBe true
+    }
+  }
+})